7f0372d523bd729a8e71de3fa2785b66529726cc
Markus Szyska <markus.szyska@codelunatics.de>
2020-07-01 08:44:44 +0200

    ...

---
+++ Tag04_Operatoren_Datentypen_II/src/demoVariablen/DemoVariablen.java
@@ -0,0 +1,32 @@
+package demoVariablen;
+
+public class DemoVariablen {
+
+	public static void main(String[] args) {
+		
+		// einzeiliger Kommentar
+		
+		/*
+		 * mehrzeiliger Kommentar
+		 */
+		
+		/**
+		 * Java Doc Kommentar
+		 * 
+		 * @author szysk
+		 * @param
+		 * @return
+		 */
+		System.out.println("Hallo Welt");
+		
+		/*
+		 * Variablen sind Wertebehaelter, die Werte wie z.B. Zahlen oder Zeichenketten
+		 * aufnehmen koennen. Ueber den Bezeichner der Variablen kann auf den Wert
+		 * zugegriffen werden. 
+		 */
+		String meinText = "Hallo Welt";
+		System.out.println(meinText);
+		
+	}
+	
+}
