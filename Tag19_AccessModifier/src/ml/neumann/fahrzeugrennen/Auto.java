8048063259447ce69827d55ec479fc2f19bc6571
Markus Szyska <markus.szyska@codelunatics.de>
2020-07-22 08:43:04 +0200

    ...

--- Tag19_AccessModifier/src/ml/neumann/fahrzeugrennen/Auto.java
+++ Tag19_AccessModifier/src/ml/neumann/fahrzeugrennen/Auto.java
@@ -1,26 +1,20 @@
- package ml.neumann.fahrzeugrennen;
+package ml.neumann.fahrzeugrennen;
 
-public class Auto extends Fahrzeug 
-{
+public class Auto extends Fahrzeug {
 
-	//Konstruktor Auto 
+	// Konstruktor Auto
 	public Auto() {
-	super();
-	this.setGeschwindigkeit(150);
-	this.setMaxGeschwindigkeit(230);
-	this.setRaeder(4);
-	
+		super();
+		this.setMaxGeschwindigkeit(230);
+		this.setGeschwindigkeit(150);		
+		this.setRaeder(4);
 	}
-	// ToString Methode 
+
+	// ToString Methode
 	public String toString() {
-		String ausgabe=super.toString(); 
-	
+		String ausgabe = super.toString();
+
 		return ausgabe;
-		}
-	
-	
+	}
+
 }
-
-	
-
-
--- Tag19_AccessModifier/src/ml/neumann/fahrzeugrennen/Rennen.java
+++ Tag19_AccessModifier/src/ml/neumann/fahrzeugrennen/Rennen.java
@@ -3,47 +3,48 @@ package ml.neumann.fahrzeugrennen;
 import java.util.Arrays;
 
 public class Rennen {
-public static void main(String[] args) {
-	Fahrzeug fr = new Fahrrad(); 
-	Fahrzeug a = new Auto(); 
-	Fahrzeug r = new Rennwagen(); 
-	Fahrzeug k = new Krankenwagen(); 
+	public static void main(String[] args) {
+		Fahrzeug fr = new Fahrrad();
+		Fahrzeug a = new Auto();
+		Fahrzeug r = new Rennwagen();
+		Fahrzeug k = new Krankenwagen();
+		
+		System.out.println(a.getMaxGeschwindigkeit());
+		
+		
+		fr.setGeschwindigkeit(20);
+		a.setGeschwindigkeit(150);
+		r.setGeschwindigkeit(200);
+		k.setGeschwindigkeit(80);
 
-	fr.setGeschwindigkeit(20);
-	a.setGeschwindigkeit(150);
-	r.setGeschwindigkeit(200);
-	k.setGeschwindigkeit(80);
-	
 // Test was in den Variablen gespeichert ist.
-	//String test = 	k.toString();
-	//System.out.println("Test:" + test);	
-	
-fr.bewegen(300);
-a.bewegen(60);
-r.bewegen(60);
-k.bewegen(60);
+		// String test = k.toString();
+		// System.out.println("Test:" + test);
+
+		fr.bewegen(300);
+		a.bewegen(60);
+		r.bewegen(60);
+		k.bewegen(60);
 
 //// Positionen nach dem Rennen 
-System.out.println("Position des Fahrrads: " + fr.getPosition());
-System.out.println("Position des Autos: " + a.getPosition());
-System.out.println("Position des Rennwagens: " + r.getPosition());
-System.out.println( "Position des Krankenwagens: " + k.getPosition());
+		System.out.println("Position des Fahrrads: " + fr.getPosition());
+		System.out.println("Position des Autos: " + a.getPosition());
+		System.out.println("Position des Rennwagens: " + r.getPosition());
+		System.out.println("Position des Krankenwagens: " + k.getPosition());
 // Array mit den Positionen 
-double[] rennerg; 
-rennerg = new double [4];
+		double[] rennerg;
+		rennerg = new double[4];
 
-rennerg[0]= fr.getPosition();
-rennerg[1]= a.getPosition();
-rennerg [2]= r.getPosition();
-rennerg[3]=k.getPosition();
+		rennerg[0] = fr.getPosition();
+		rennerg[1] = a.getPosition();
+		rennerg[2] = r.getPosition();
+		rennerg[3] = k.getPosition();
 
 // sortieren der Positionen
-Arrays.sort(rennerg);
-System.out.println("Der Gewinner und der Verlierer des Rennens -------");
-System.out.println("Der Gewinner: " + rennerg[3]);
-System.out.println("Der Verlierer: " + rennerg[0]);
-}
-
- 
+		Arrays.sort(rennerg);
+		System.out.println("Der Gewinner und der Verlierer des Rennens -------");
+		System.out.println("Der Gewinner: " + rennerg[3]);
+		System.out.println("Der Verlierer: " + rennerg[0]);
+	}
 
 }
