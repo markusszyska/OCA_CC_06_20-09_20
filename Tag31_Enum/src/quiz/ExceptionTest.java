17ed6d84a1c52eac5f6c5b66c58778da991c357b
Your Full Name <markus.szyska@codelunatics.de>
2020-08-07 13:00:33 +0200

    ...

---
+++ Tag31_Enum/src/quiz/ExceptionTest.java
@@ -0,0 +1,26 @@
+package quiz;
+
+class NewException extends Exception {
+}
+
+class AnotherException extends Exception {
+}
+
+public class ExceptionTest {
+	public static void main(String[] args) throws Exception {
+		try {
+			m2();
+		} finally {
+			System.out.println("Hallo");
+//			m3();
+		}
+	}
+
+	public static void m2() throws NewException {
+		throw new NewException();
+	}
+
+	public static void m3() throws AnotherException {
+		throw new AnotherException();
+	}
+}
---
+++ Tag31_Enum/src/quiz/TestClass.java
@@ -0,0 +1,24 @@
+package quiz;
+
+public class TestClass {
+	public void method(Object o) {
+		System.out.println("Object Version");
+	}
+
+	public void method(java.io.FileNotFoundException s) {
+		System.out.println("java.io.FileNotFoundException Version");
+	}
+
+	public void method(java.io.IOException s) {
+		System.out.println("IOException Version");
+	}
+
+//	public void method(String s) {
+//		System.out.println("IOException Version");
+//	}
+
+	public static void main(String args[]) {
+		TestClass tc = new TestClass();
+		tc.method(null);
+	}
+}
